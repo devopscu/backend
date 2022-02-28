5217cc98b40ec205e2c146e9c03ab6c35e96394d
Murat Akdeniz <makdeniz@msn.com>
2022-02-28 20:22:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+var db = mongoose.connection;
+mongoose.connect(process.env.MongoURL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+db.on("connecting", function () {
+  console.log("connecting to MongoDB...");
+})
+  .on("error", function (error) {
+    console.error("Error in MongoDb connection: " + error);
+    mongoose.disconnect();
+  })
+  .on("connected", function () {
+    console.log("MongoDB connected!");
+  })
+  .once("open", function () {
+    console.log("MongoDB connection opened");
+  })
+  .on("reconnected", function () {
+    console.log("MongoDB reconnected!");
+  })
+  .on("disconnected", function () {
+    console.log("MongoDB disconnected!");
+    mongoose.connect(config.MongoURL(), {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/login", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
